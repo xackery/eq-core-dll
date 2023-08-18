96c1c55c16463cdfc2f35ef991346c42bf516eb3
Xackery <xackery@gmail.com>
2023-08-18 13:35:36 -0700

    Fix NPC injector, force uppercase

--- src/core_npc.h
+++ src/core_npc.h
@@ -7,11 +7,14 @@
 char* __fastcall InjectCustomNPCs_Trampoline(char* pThis, char* pPtr, int raceID, int genderID, const char* modelName, int raceMask, int dbStrID);
 char* __fastcall InjectCustomNPCs_Detour(char*pThis, char* pPtr, int raceID, int genderID, const char* modelName, int raceMask, int dbStrID)
 {
+	
+	
 	if (!raceID) return InjectCustomNPCs_Trampoline(pThis, pPtr, raceID, genderID, modelName, raceMask, dbStrID);
 	if (raceID == 723) {
 		for (auto&& npc : NPCs) {
-			DebugSpew("injecting race %s gender %d id %d", npc.modelName , npc.genderID, npc.raceID);
-			InjectCustomNPCs_Trampoline(pThis, pPtr, npc.raceID, npc.genderID, npc.modelName, npc.raceMask, npc.dbStrID);
+			char* modelNameUpper = _strdup(npc.modelName);
+			DebugSpew("injecting race %s gender %d id %d", modelNameUpper, npc.genderID, npc.raceID);
+			InjectCustomNPCs_Trampoline(pThis, pPtr, npc.raceID, npc.genderID, modelNameUpper, npc.raceMask, npc.dbStrID);
 		}
 	}
 	DebugSpew("loaded race %s gender %d as id %d", modelName, genderID, raceID);
