e4b2ec8fd9f1d5385ffe28d6ffb50db9ab5a3ff7
xackery <xackery@gmail.com>
2024-05-02 10:01:06 -0700

    Disable accidentaly enabled areCustomOldAnimationsEnabled

--- src/_options.h
+++ src/_options.h
@@ -78,11 +78,11 @@ static NPCEntry NPCs[] = {
 
 // areCustomAnimationsEnabled if set to true will allow custom animations defined in Animations[] to be injected in game
 // NOTE: Must be exactly 2 or 3 characters long, e.g. "BET" or "OK", if only 2 characters, it will replace the first 2 characters of the animation
-bool areCustomOldAnimationsEnabled = true;
+bool areCustomOldAnimationsEnabled = false;
 static AnimationEntry CustomAnimations[] = {
     // originalName, replacementName
-    AnimationEntry("DA", "DW"),
-    AnimationEntry("OK", "EL"),
+    //AnimationEntry("DA", "DW"),
+    //AnimationEntry("OK", "EL"),
 };
 
 // ***** ZONE *******
