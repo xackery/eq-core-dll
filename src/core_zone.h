9bf8c18d10901729c83cfc51d88e6878b13881eb
xackery <xackery@gmail.com>
2024-07-05 00:27:15 -0700

    Fix add/replace zone support

--- src/core_zone.h
+++ src/core_zone.h
@@ -6,11 +6,12 @@
 
 static bool isCustomZonesReplacingExisting = false;
 static vector<int> zonesToInject;
+static bool isEndOfZones = false;
  
 char* __fastcall InjectCustomZones_Trampoline(char* pThis, char* pPtr, unsigned __int32 zoneType, unsigned __int32 zoneID, char* zoneShortName, char* zoneLongName, unsigned __int32 eqStrID, __int32 zoneFlags2, __int32 x, __int32 y, __int32 z);
 char* __fastcall InjectCustomZones_Detour(char* pThis, char* pPtr, unsigned __int32 zoneType, unsigned __int32 zoneID, char* zoneShortName, char* zoneLongName, unsigned __int32 eqStrID, __int32 zoneFlags2, __int32 x, __int32 y, __int32 z)
 { 
-	if (isCustomZonesReplacingExisting) {
+	if (!isEndOfZones && isCustomZonesReplacingExisting) {
 		for (auto&& zone : Zones) {
 			if (zoneID != zone.zoneID) {
 				continue;
@@ -20,22 +21,35 @@ char* __fastcall InjectCustomZones_Detour(char* pThis, char* pPtr, unsigned __in
 			return InjectCustomZones_Trampoline(pThis, pPtr, zone.zoneType, zone.zoneID, zone.zoneShortName, zone.zoneLongName, zone.eqStrID, zone.zoneFlags2, zone.x, zone.y, zone.z);
 		}
 	}
+	if (!isEndOfZones) {
+		DebugSpew("original zone %s id %d", zoneShortName, zoneID);
+	}
+	return InjectCustomZones_Trampoline(pThis, pPtr, zoneType, zoneID, zoneShortName, zoneLongName, eqStrID, zoneFlags2, x, y, z);
+}
+
+DETOUR_TRAMPOLINE_EMPTY(char* __fastcall InjectCustomZones_Trampoline(char* pThis, char* pPtr, unsigned __int32 zoneType, unsigned __int32 zoneID, char* zoneShortName, char* zoneLongName, unsigned __int32 eqStrID, __int32 zoneFlags2, __int32 x, __int32 y, __int32 z));
+
+
+char* __fastcall InjectCustomZonesAdd_Trampoline(char* pThis, char* pPtr, unsigned __int32 zoneType, unsigned __int32 zoneID, char* zoneShortName, char* zoneLongName, unsigned __int32 eqStrID, __int32 zoneFlags2, __int32 x, __int32 y, __int32 z);
+char* __fastcall InjectCustomZonesAdd_Detour(char* pThis, char* pPtr, unsigned __int32 zoneType, unsigned __int32 zoneID, char* zoneShortName, char* zoneLongName, unsigned __int32 eqStrID, __int32 zoneFlags2, __int32 x, __int32 y, __int32 z)
+{
 	if (!strcmp(zoneShortName, "interiorwalltest")) {
+		DebugSpew("reached end of zone loading, now adding any custom zones");
+		isEndOfZones = true;
 		for (auto&& zoneID : zonesToInject) {
 			for (auto&& zone : Zones) {
 				if (zoneID != zone.zoneID) {
 					continue;
 				}
-				DebugSpew("injecting zone %s id %d", zone.zoneShortName, zone.zoneID);
-				InjectCustomZones_Trampoline(pThis, pPtr, zone.zoneType, zone.zoneID, zone.zoneShortName, zone.zoneLongName, zone.eqStrID, zone.zoneFlags2, zone.x, zone.y, zone.z);
+				DebugSpew("adding zone %s id %d", zone.zoneShortName, zone.zoneID);
+				InjectCustomZonesAdd_Trampoline(pThis, pPtr, zone.zoneType, zone.zoneID, zone.zoneShortName, zone.zoneLongName, zone.eqStrID, zone.zoneFlags2, zone.x, zone.y, zone.z);
 			}
 		}
 	}
-	DebugSpew("loaded zone %s id %d", zoneShortName, zoneID);
-	return InjectCustomZones_Trampoline(pThis, pPtr, zoneType, zoneID, zoneShortName, zoneLongName, eqStrID, zoneFlags2, x, y, z);
+	return InjectCustomZonesAdd_Trampoline(pThis, pPtr, zoneType, zoneID, zoneShortName, zoneLongName, eqStrID, zoneFlags2, x, y, z);
 }
 
-DETOUR_TRAMPOLINE_EMPTY(char* __fastcall InjectCustomZones_Trampoline(char* pThis, char* pPtr, unsigned __int32 zoneType, unsigned __int32 zoneID, char* zoneShortName, char* zoneLongName, unsigned __int32 eqStrID, __int32 zoneFlags2, __int32 x, __int32 y, __int32 z));
+DETOUR_TRAMPOLINE_EMPTY(char* __fastcall InjectCustomZonesAdd_Trampoline(char* pThis, char* pPtr, unsigned __int32 zoneType, unsigned __int32 zoneID, char* zoneShortName, char* zoneLongName, unsigned __int32 eqStrID, __int32 zoneFlags2, __int32 x, __int32 y, __int32 z));
 
 // Hooks to EQWorldData::AddZone
 void InjectCustomZones() {
@@ -46,6 +60,8 @@ void InjectCustomZones() {
 		}
 		isCustomZonesReplacingExisting = true;
 	}
+	isEndOfZones = false;
+	EzDetour((((DWORD)0x007DC430 - 0x400000) + baseAddress), InjectCustomZonesAdd_Detour, InjectCustomZonesAdd_Trampoline);
 	EzDetour((((DWORD)0X007DC290 - 0x400000) + baseAddress), InjectCustomZones_Detour, InjectCustomZones_Trampoline); 
 };
 //bool __thiscall sub_7DC430(_DWORD *this, int a2, int a3, char *a4, char *a5, int a6, int a7, int a8, int a9, int a10)
